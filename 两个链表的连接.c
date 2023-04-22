5b3884213bef00558d181acce6a212346c755bed
shouzhuchuxin <122348136+shouzhuchuxin@users.noreply.github.com>
2023-04-22 21:20:44 +0800

    Update 两个链表的连接.c

--- 两个链表的连接.c
+++ 两个链表的连接.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+//创建结构体包含数据域和指针域
  struct Lnode
  {
  	int data;
@@ -7,22 +8,23 @@
  	
  };
  
- struct Lnode*p1=NULL;
-  struct Lnode*p2=NULL;
-   struct Lnode*p=NULL;
-    struct Lnode*q1=NULL;
-     struct Lnode*q2=NULL;
-     struct Lnode*p3=NULL;
-     struct Lnode*Q=NULL;
+ struct Lnode*p1=NULL;//第一个链表的头节点
+  struct Lnode*p2=NULL;//第二个
+   struct Lnode*p=NULL;//开辟动态空间使用的指针
+    struct Lnode*q1=NULL;//第一个链表中指向始终指向最后一个节点的指针，用来进行链表的链接
+     struct Lnode*q2=NULL;//第二个
+     struct Lnode*p3=NULL;//这个是进行两个链表链接时，新创建的链表的跟随指针，用来进行链表的链接
+     struct Lnode*Q=NULL;//这个是上面那个指针的头节点
      
  
 int main()
 {
 	
 	 int i=0,j=0;
- 
+ //这里注意：当创建的链表有头节点时记得对头节点创建空间
    p1=(struct Lnode*)malloc(sizeof(struct Lnode));
    p2=(struct Lnode*)malloc(sizeof(struct Lnode));
+	//对第一个链表进行赋值
  while(i<=3)
 {
 	p=( struct Lnode*)malloc(sizeof(struct Lnode));
@@ -46,6 +48,7 @@ int main()
 	
  } 
  printf("\n");
+	//对第二个链表进行赋值
  while(j<=7)
  {
  	p=( struct Lnode*)malloc(sizeof(struct Lnode));
@@ -65,6 +68,7 @@ int main()
 		printf("%d",q2->data);
  }
  printf("\n");
+	//使新创建的头节点p3以p1的位置为起点进行后续链表的链接
  p3=Q=p1;
  p1=p1->next;
  p2=p2->next;
@@ -109,6 +113,7 @@ int main()
  	p2=p2->next;
  
  }
+	//注意Q的指向
  Q=Q->next;
  while(Q)
  {
